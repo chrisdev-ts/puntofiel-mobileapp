7bddddecf5baa45362c8ee86a5e0b6ff35d2513d
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-10-08 21:27:35 -0600

    feat: add main barrel exports for clean architecture

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Barrel principal - Estructura Clean Architecture
+// Exporta las capas principales del proyecto
+
+export * from "./core";
+export * from "./infrastructure";
+export * from "./presentation";
