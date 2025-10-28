a75e1449c8425b61be6bcfe7815a0514faa0f8dc
Juan Madrid <guero_jdmo@hotmail.com>
2025-10-27 22:54:22 -0600

    defaultFormatter & index.tsx solved

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,5 @@
-/*import { WelcomeScreen } from "@/src/presentation/screens/Auth/WelcomeScreen";
+import { WelcomeScreen } from "@/src/presentation/screens/Auth/WelcomeScreen";
 
 export default function HomeScreen() {
 	return <WelcomeScreen />;
-}*/
-
-import RewardsIndexScreen from "@/src/presentation/screens/reward/RewardsIndexScreen";
-
-export default function RewardsIndexRoute() {
-	return <RewardsIndexScreen />;
 }
