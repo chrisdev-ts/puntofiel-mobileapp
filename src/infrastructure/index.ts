31df37fb2693fc9635f6c681090c4dfb05acfc3f
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-10-29 05:21:41 -0600

    fix: update app config and add dev environment config

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,5 +1,6 @@
 // Barrel exports para la capa de infraestructura
 
+export { IS_DEV_MODE, MOCK_USER_ID } from "./config/dev";
 export { env } from "./config/env";
 export { queryClient } from "./config/queryClient";
 export { SupabaseLoyaltyRepository } from "./repositories/SupabaseLoyaltyRepository";
