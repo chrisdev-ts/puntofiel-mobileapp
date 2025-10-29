7c255e53f9d48eede4461cc85246f92609bab7ff
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-10-29 05:20:26 -0600

    feat: update reward hooks (useReward, useRewardDetail)

--- src/presentation/hooks/useRewardDetail.ts
+++ src/presentation/hooks/useRewardDetail.ts
@@ -1,15 +1,15 @@
-import { GetRewardByIdUseCase } from '@/src/core/usecases/reward/GetRewardByIdUseCase';
-import { SupabaseRewardRepository } from '@/src/infrastructure/repositories/SupabaseRewardRepository';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
+import { GetRewardByIdUseCase } from "@/src/core/usecases/reward/GetRewardByIdUseCase";
+import { SupabaseRewardRepository } from "@/src/infrastructure/repositories/SupabaseRewardRepository";
 
 const rewardRepository = new SupabaseRewardRepository();
 const getRewardByIdUseCase = new GetRewardByIdUseCase(rewardRepository);
 
 export function useRewardDetail(rewardId: string | undefined) {
-  return useQuery({
-    queryKey: ['reward', rewardId],
-    queryFn: () => getRewardByIdUseCase.execute(rewardId as string),
-    enabled: !!rewardId,
-    staleTime: 1000 * 60 * 5,
-  });
+	return useQuery({
+		queryKey: ["reward", rewardId],
+		queryFn: () => getRewardByIdUseCase.execute(rewardId as string),
+		enabled: !!rewardId,
+		staleTime: 1000 * 60 * 5,
+	});
 }
