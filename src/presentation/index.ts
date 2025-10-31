087bca226ae6dde1fa17b3450d09b3d16a11c112
chrisdev-ts <christian.serrano.puertos@gmail.com>
2025-10-31 02:00:40 -0600

    refactor: update presentation exports and fix WelcomeScreen import

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,4 +1,5 @@
 export { useAuth } from "./hooks/useAuth";
 export { LoginScreen } from "./screens/auth/LoginScreen";
 export { RegisterScreen } from "./screens/auth/RegisterScreen";
-export { WelcomeScreen } from "./screens/auth/WelcomeScreen";
+export { default as WelcomeScreen } from "./screens/auth/WelcomeScreen";
+export { useAuthStore } from "./stores/authStore";
